c00d3b49d8cd535389265e0f5f4c5a51ab1ae28b
MathGaron <mathieugaron91@gmail.com>
2019-08-06 22:49:54 -0700

    receives and treat the gradients in closed loops

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import numpy as np
+import pyaudio
 import torch
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 from tqdm import tqdm
 from networks import Net
-import os
-import pickle
-
-from load_gradients import test
 
 
 def allocate_memory(model, n_samples):
@@ -36,9 +33,57 @@ def process(model_orig, device, train_loader, optimizer, gradient_func, n_steps=
         optimizer.step()
 
 
-def gradient_function(gradient):
-    # Todo : audio encoding and decoding here
-    return gradient.astype(np.float16)
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    accuracy = 100. * correct / len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset), accuracy))
+    return test_loss, accuracy
+
+
+class GradientFunc:
+    def __init__(self):
+        self.p = pyaudio.PyAudio()
+        self.fs = 44100
+        self.duration = 0.03
+        self.f = 300.0
+        self.stream = self.p.open(format=self.p.get_format_from_width(4),
+                        channels=1,
+                        rate=self.fs,
+                        output=True)
+
+    def __del__(self):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.p.terminate()
+
+    @staticmethod
+    def generate_tone(fs, freq, duration):
+        npsin = np.sin(2 * np.pi * np.arange(fs * duration) * freq / fs)
+        samples = npsin.astype(np.float32)
+        return 0.1 * samples
+
+    def __call__(self, gradient):
+
+        norm_grad = np.linalg.norm(gradient)
+        tone = self.f + (norm_grad * 500.0)
+        samples = self.generate_tone(self.fs, tone, self.duration).astype(np.float32)
+        # here we write to audio or wav file
+        self.stream.write(samples.flatten().tobytes())
+
+        return gradient
 
 
 def run_main():
@@ -66,10 +111,10 @@ def run_main():
     model.train()
 
     optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
-
+    grad_func = GradientFunc()
     training_curve = []
     for epoch in range(1, 6):
-        process(model, device, train_loader, optimizer, gradient_function, n_steps=100)
+        process(model, device, train_loader, optimizer, grad_func, n_steps=100)
         # save epoch gradients
         # save test results
         loss, accuracy = test(model, device, test_loader)
